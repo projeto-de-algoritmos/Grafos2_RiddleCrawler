5990a449b9b7e31f3fbee73e80472d5686c78981
Pedro Henrique <pedrodsm1819@gmail.com>
2023-10-15 15:34:36 -0300

    Adicionando músicas no jogo e começando cenários

--- Riddle_Crawler.py
+++ Riddle_Crawler.py
@@ -1,5 +1,22 @@
 import pyxel
 
+SCENE_TITLE = 0
+SCENE_PLAY = 1
+SCENE_GAMEOVER = 2
+
+def load_bgm(msc, filename, snd1, snd2, snd3):
+    # Loads a json file for 8bit BGM generator by frenchbread.
+    # Each track is stored in snd1, snd2 and snd3 of the sound
+    # respectively and registered in msd of the music.
+    import json
+
+    with open(filename, "rt") as file:
+        bgm = json.loads(file.read())
+        pyxel.sound(snd1).set(*bgm[0])
+        pyxel.sound(snd2).set(*bgm[1])
+        pyxel.sound(snd3).set(*bgm[2])
+        pyxel.music(msc).set([snd1], [snd2], [snd3], [])
+
 # Botão 
 class Botao:
     def __init__(self, x, y):
@@ -21,17 +38,30 @@ class Botao:
         return self.x <= x <= self.x + self.w and self.y <= y <= self.y + self.h
 
 
+# 
 class App:
     def __init__(self):
-        pyxel.init(160, 120, title="Jogo de Escolhas", fps=60)
+        pyxel.init(160, 120, title="Riddle Crawler", fps=60)
         pyxel.load("assets/riddle.pyxres")
         self.botao = Botao(50, 50)    
-        self.far_cloud = [(-10, 75), (40, 65), (90, 60)]
+        # self.far_cloud = [(-10, 75), (40, 65), (90, 60)]
         self.near_cloud = [(10, 25), (70, 35), (120, 15)]
 
+        # Carregando Música do Jogo
+        pyxel.sound(0).set("a3a2c1a1", "p", "7", "s", 5)
+        pyxel.sound(1).set("a3a2c2c2", "n", "7742", "s", 10)
+        load_bgm(0, "assets/bgm_title.json", 2, 3, 4)
+        load_bgm(1, "assets/bgm_play.json", 5, 6, 7)
+
+        # Começando na tela inicial
+        self.scene = SCENE_TITLE
+
+        # Carregando música, mouse e o jogo
+        pyxel.playm(0, loop=True)
         pyxel.mouse(True)
         pyxel.run(self.update, self.draw)
 
+    # FUNÇÃO QUE ATUALIZA O QUE OCORRE NO JOGO
     def update(self):
         if pyxel.btnp(pyxel.KEY_Q):
             pyxel.quit()
@@ -41,7 +71,23 @@ class App:
             if self.botao.in_button(pyxel.mouse_x, pyxel.mouse_y):
                 self.botao.clicked = True
         
+        # Atualização da tela inicial para tela de jogo e gameover
+        self.background.update()
+        if self.scene == SCENE_TITLE:
+            self.update_title_scene()
+        elif self.scene == SCENE_PLAY:
+            self.update_play_scene()
+        elif self.scene == SCENE_GAMEOVER:
+            self.update_gameover_scene()
 
+        def update_title_scene(self):
+            if pyxel.btnp(pyxel.KEY_RETURN) or pyxel.btnp(pyxel.GAMEPAD1_BUTTON_X):
+                self.scene = SCENE_PLAY
+
+        def update_play_scene(self):
+            pass
+
+    # FUNÇÃO QUE DESENHA O JOGO E O CENÁRIO
     def draw(self):
      
         pyxel.cls(12)
@@ -62,14 +108,16 @@ class App:
 
         # Desenha nuvens
         offset = (pyxel.frame_count // 16) % 160
-        for i in range(2):
-            for x, y in self.far_cloud:
-                pyxel.blt(x + i * 160 - offset, y, 0, 64, 32, 32, 8, 12)
+        # for i in range(2):
+        #     for x, y in self.far_cloud:
+        #         pyxel.blt(x + i * 160 - offset, y, 0, 64, 32, 32, 8, 12)
+
         offset = (pyxel.frame_count // 8) % 160
         for i in range(2):
             for x, y in self.near_cloud:
                 pyxel.blt(x + i * 160 - offset, y, 0, 0, 32, 56, 8, 12)
 
+        
         if self.botao.clicked:
             pyxel.text(self.botao.x - 30, self.botao.y + 15, "Voce clicou no botao", 1)
 
