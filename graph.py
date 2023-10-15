abd37bffd92523fbbd782a7af899e893493a7b80
João Pedro Morbeck <jpmorbeck45@gmail.com>
2023-10-15 14:09:58 -0300

    ajustando a geração de grafos para resultados mais constantes

--- graph.py
+++ graph.py
@@ -12,6 +12,10 @@ def randomWeightedGraph(num_nodes, num_edges, min_weight, max_weight):
     while num_edges > 0:
         node1, node2 = random.sample(range(num_nodes), 2)
 
+        # Impede a criação de aresta entre 0 e 14
+        if node1 == 0 and node2 == 14 or node1 == 14 and node2 == 0:
+            continue
+        
         if node2 not in graph[node1]:
             weight = random.randint(min_weight, max_weight)
             graph[node1][node2] = weight
@@ -54,3 +58,31 @@ def dijkstra(graph, start_node, end_node):
         end_node = previous_nodes.get(end_node, None)
 
     return path, distances
+
+# Testing the graphs functions
+'''
+if __name__ == "__main__":
+    num_nodes = 15
+    num_edges = num_nodes + int(num_nodes / 2)
+    min_weight = 10
+    max_weight = 17
+    start_node = 0
+    end_node = 14
+    
+    shortest_path = [14]
+
+    # Loop to create a graph that has a path between 0 and 14
+    while(shortest_path == [14]):
+        random_graph = randomWeightedGraph(num_nodes, num_edges, min_weight, max_weight)
+        shortest_path, shortest_distance = dijkstra(random_graph, start_node, end_node)
+    
+    # Print the edges and their weights
+    for node1, edges in random_graph.items():
+        for node2, weight in edges.items():
+            print(f"Edge ({node1}, {node2}) has weight {weight}")
+
+    print(f"Nodes: {num_nodes} Edges: {num_edges}")
+    print(f"Shortest path from {start_node} to {end_node}: {shortest_path}")
+    print(f"Shortest distance from {start_node} to {end_node}: {shortest_distance}")
+'''
+        
